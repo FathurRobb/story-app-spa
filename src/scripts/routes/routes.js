f69984a930bfed92452ea15049224b252ce43f02
fathurrobb <ilhamfathurrobbani@gmail.com>
2025-07-13 16:24:37 +0700

    feature: add another page to routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -7,6 +7,7 @@ import LoginPage from "../pages/auth/login/login-page";
 import RegisterPage from "../pages/auth/register/register-page";
 import StoryDetailPage from "../pages/story-detail/story-detail-page";
 import NewPage from "../pages/new/new-page";
+import BookmarkPage from "../pages/bookmark/bookmark-page";
 
 const routes = {
   "/login": () => checkUnauthenticatedRouteOnly(new LoginPage()),
@@ -15,6 +16,7 @@ const routes = {
   "/": () => checkAuthenticatedRoute(new HomePage()),
   "/stories/:id": () => checkAuthenticatedRoute(new StoryDetailPage()),
   "/new": () => checkAuthenticatedRoute(new NewPage()),
+  "/bookmark": () => checkAuthenticatedRoute(new BookmarkPage()),
 };
 
 export default routes;
