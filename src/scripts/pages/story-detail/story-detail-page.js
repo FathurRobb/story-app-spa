df57bc9a1467a4c13614776a702d5cbbcd6c0249
fathurrobb <ilhamfathurrobbani@gmail.com>
2025-07-13 16:23:59 +0700

    featureL add component to save & remove story to indexedDB

--- src/scripts/pages/story-detail/story-detail-page.js
+++ src/scripts/pages/story-detail/story-detail-page.js
@@ -2,11 +2,14 @@ import {
   generateLoaderAbsoluteTemplate,
   generateStoryDetailErrorTemplate,
   generateStoryDetailTemplate,
+  generateSaveStoryButtonTemplate,
+  generateRemoveStoryButtonTemplate,
 } from "../../templates";
 import StoryDetailPresenter from "./story-detail-presenter";
 import { parseActivePathname } from "../../routes/url-parser";
 import * as StoryAPI from "../../data/api";
 import Map from "../../utils/map";
+import Database from "../../data/database";
 
 export default class StoryDetailPage {
   #presenter = null;
@@ -27,6 +30,7 @@ export default class StoryDetailPage {
     this.#presenter = new StoryDetailPresenter(parseActivePathname().id, {
       view: this,
       apiModel: StoryAPI,
+      dbModel: Database,
     });
 
     this.#presenter.showStoryDetail();
@@ -53,6 +57,8 @@ export default class StoryDetailPage {
       this.#map.changeCamera(storyCoordinate);
       this.#map.addMarker(storyCoordinate, markerOptions, popupOptions);
     }
+
+    await this.#presenter.showSaveButton();
   }
 
   populateStoryDetailError(message) {
@@ -82,5 +88,43 @@ export default class StoryDetailPage {
 
   hideMapLoading() {
     document.getElementById("map-loading-container").innerHTML = "";
+  }
+
+  renderSaveButton() {
+    document.getElementById("save-actions-container").innerHTML =
+      generateSaveStoryButtonTemplate();
+
+    document
+      .getElementById("story-detail-save")
+      .addEventListener("click", async () => {
+        await this.#presenter.saveStory();
+        await this.#presenter.showSaveButton();
+      });
+  }
+
+  saveToBookmarkSuccessfully(message) {
+    console.log(message);
+  }
+  saveToBookmarkFailed(message) {
+    alert(message);
+  }
+
+  renderRemoveButton() {
+    document.getElementById("save-actions-container").innerHTML =
+      generateRemoveStoryButtonTemplate();
+
+    document
+      .getElementById("story-detail-remove")
+      .addEventListener("click", async () => {
+        await this.#presenter.removeStory();
+        await this.#presenter.showSaveButton();
+      });
+  }
+
+  removeFromBookmarkSuccessfully(message) {
+    console.log(message);
+  }
+  removeFromBookmarkFailed(message) {
+    alert(message);
   }
 }
